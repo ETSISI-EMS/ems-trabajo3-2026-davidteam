0792b3a92afafb6ef59bf960c9f773c263783cec
davidpelaez19 <david.pelaez@alumnos.upm.es>
2026-04-21 13:04:32 +0200

    No duplicado en Localizacion. #1

--- src/main/java/com/practica/ems/covid/Localizacion.java
+++ src/main/java/com/practica/ems/covid/Localizacion.java
@@ -1,6 +1,5 @@
 package com.practica.ems.covid;
 
-
 import java.util.Iterator;
 import java.util.LinkedList;
 
@@ -10,117 +9,133 @@ import com.practica.genericas.FechaHora;
 import com.practica.genericas.PosicionPersona;
 
 public class Localizacion {
-	LinkedList<PosicionPersona> lista;
+    LinkedList<PosicionPersona> lista;
 
-	public Localizacion() {
-		super();
-		this.lista = new LinkedList<PosicionPersona>();
-	};
-	
-	public LinkedList<PosicionPersona> getLista() {
-		return lista;
-	}
+    public Localizacion() {
+        super();
+        this.lista = new LinkedList<PosicionPersona>();
+    }
 
-	public void setLista(LinkedList<PosicionPersona> lista) {
-		this.lista = lista;
-	}
+    public LinkedList<PosicionPersona> getLista() {
+        return lista;
+    }
 
-	public void addLocalizacion (PosicionPersona p) throws EmsDuplicateLocationException {
-		try {
-			findLocalizacion(p.getDocumento(), p.getFechaPosicion().getFecha().toString(),p.getFechaPosicion().getHora().toString() );
-			throw new EmsDuplicateLocationException();
-		}catch(EmsLocalizationNotFoundException e) {
-			lista.add(p);
-		}
-	}
-	
-	public int findLocalizacion (String documento, String fecha, String hora) throws EmsLocalizationNotFoundException {
-	    int cont = 0;
-	    Iterator<PosicionPersona> it = lista.iterator();
-	    while(it.hasNext()) {
-	    	cont++;
-	    	PosicionPersona pp = it.next();
-	    	FechaHora fechaHora = this.parsearFecha(fecha, hora);
-	    	if(pp.getDocumento().equals(documento) && 
-	    	   pp.getFechaPosicion().equals(fechaHora)) {
-	    		return cont;
-	    	}
-	    } 
-	    throw new EmsLocalizationNotFoundException();
-	}
-	public void delLocalizacion(String documento, String fecha, String hora) throws EmsLocalizationNotFoundException {
-	    int pos=-1;
-	    int i;
-	    /**
-	     *  Busca la localización, sino existe lanza una excepción
-	     */
-	    try {
-			pos = findLocalizacion(documento, fecha, hora);
-		} catch (EmsLocalizationNotFoundException e) {
-			throw new EmsLocalizationNotFoundException();
-		}
-	    this.lista.remove(pos);
-	    
-	}
-	
-	void printLocalizacion() {    
-	    for(int i = 0; i < this.lista.size(); i++) {
-	        System.out.printf("%d;%s;", i, lista.get(i).getDocumento());
-	        FechaHora fecha = lista.get(i).getFechaPosicion();        
-	        System.out.printf("%02d/%02d/%04d;%02d:%02d;", 
-	        		fecha.getFecha().getDia(), 
-	        		fecha.getFecha().getMes(), 
-	        		fecha.getFecha().getAnio(),
-	        		fecha.getHora().getHora(),
-	        		fecha.getHora().getMinuto());
-	        System.out.printf("%.4f;%.4f\n", lista.get(i).getCoordenada().getLatitud(), 
-	        		lista.get(i).getCoordenada().getLongitud());
-	    }
-	}
+    public void setLista(LinkedList<PosicionPersona> lista) {
+        this.lista = lista;
+    }
 
-	@Override
-	public String toString() {
-		String cadena = "";
-		for(int i = 0; i < this.lista.size(); i++) {
-			PosicionPersona pp = lista.get(i);
-	        cadena += String.format("%s;", pp.getDocumento());
-	        FechaHora fecha = pp.getFechaPosicion();        
-	        cadena+=String.format("%02d/%02d/%04d;%02d:%02d;", 
-	        		fecha.getFecha().getDia(), 
-	        		fecha.getFecha().getMes(), 
-	        		fecha.getFecha().getAnio(),
-	        		fecha.getHora().getHora(),
-	        		fecha.getHora().getMinuto());
-	        cadena+=String.format("%.4f;%.4f\n", pp.getCoordenada().getLatitud(), 
-	        		pp.getCoordenada().getLongitud());
-	    }
-		
-		return cadena;		
-	}
-	
-	@SuppressWarnings("unused")
-	private FechaHora parsearFecha (String fecha) {
-		int dia, mes, anio;
-		String[] valores = fecha.split("\\/");
-		dia = Integer.parseInt(valores[0]);
-		mes = Integer.parseInt(valores[1]);
-		anio = Integer.parseInt(valores[2]);
-		FechaHora fechaHora = new FechaHora(dia, mes, anio, 0, 0);
-		return fechaHora;
-	}
-	
-	private  FechaHora parsearFecha (String fecha, String hora) {
-		int dia, mes, anio;
-		String[] valores = fecha.split("\\/");
-		dia = Integer.parseInt(valores[0]);
-		mes = Integer.parseInt(valores[1]);
-		anio = Integer.parseInt(valores[2]);
-		int minuto, segundo;
-		valores = hora.split("\\:");
-		minuto = Integer.parseInt(valores[0]);
-		segundo = Integer.parseInt(valores[1]);
-		FechaHora fechaHora = new FechaHora(dia, mes, anio, minuto, segundo);
-		return fechaHora;
-	}
-	
+    public void addLocalizacion(PosicionPersona p) throws EmsDuplicateLocationException {
+        try {
+            findLocalizacion(p.getDocumento(), formatearFecha(p), formatearHora(p));
+            throw new EmsDuplicateLocationException();
+        } catch (EmsLocalizationNotFoundException e) {
+            lista.add(p);
+        }
+    }
+
+    public int findLocalizacion(String documento, String fecha, String hora) throws EmsLocalizationNotFoundException {
+        int cont = 0;
+        Iterator<PosicionPersona> it = lista.iterator();
+        while (it.hasNext()) {
+            cont++;
+            PosicionPersona pp = it.next();
+            FechaHora fechaHora = parsearFecha(fecha, hora);
+            if (pp.getDocumento().equals(documento) &&
+                    pp.getFechaPosicion().equals(fechaHora)) {
+                return cont;
+            }
+        }
+        throw new EmsLocalizationNotFoundException();
+    }
+
+    public void delLocalizacion(String documento, String fecha, String hora) throws EmsLocalizationNotFoundException {
+        int pos = -1;
+        try {
+            pos = findLocalizacion(documento, fecha, hora);
+        } catch (EmsLocalizationNotFoundException e) {
+            throw new EmsLocalizationNotFoundException();
+        }
+        this.lista.remove(pos);
+    }
+
+    void printLocalizacion() {
+        for (int i = 0; i < this.lista.size(); i++) {
+            imprimirLinea(i, lista.get(i));
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder cadena = new StringBuilder();
+        for (int i = 0; i < this.lista.size(); i++) {
+            cadena.append(formatearLinea(lista.get(i)));
+        }
+        return cadena.toString();
+    }
+
+    private void imprimirLinea(int index, PosicionPersona pp) {
+        System.out.printf("%d;%s;", index, pp.getDocumento());
+        System.out.printf("%s;", formatearFechaHora(pp));
+        System.out.printf("%.4f;%.4f\n",
+                pp.getCoordenada().getLatitud(),
+                pp.getCoordenada().getLongitud());
+    }
+
+    private String formatearLinea(PosicionPersona pp) {
+        return String.format("%s;%s;%.4f;%.4f\n",
+                pp.getDocumento(),
+                formatearFechaHora(pp),
+                pp.getCoordenada().getLatitud(),
+                pp.getCoordenada().getLongitud());
+    }
+
+    private String formatearFechaHora(PosicionPersona pp) {
+        FechaHora fecha = pp.getFechaPosicion();
+        return String.format("%02d/%02d/%04d;%02d:%02d",
+                fecha.getFecha().getDia(),
+                fecha.getFecha().getMes(),
+                fecha.getFecha().getAnio(),
+                fecha.getHora().getHora(),
+                fecha.getHora().getMinuto());
+    }
+
+    private String formatearFecha(PosicionPersona pp) {
+        FechaHora fecha = pp.getFechaPosicion();
+        return String.format("%02d/%02d/%04d",
+                fecha.getFecha().getDia(),
+                fecha.getFecha().getMes(),
+                fecha.getFecha().getAnio());
+    }
+
+    private String formatearHora(PosicionPersona pp) {
+        FechaHora fecha = pp.getFechaPosicion();
+        return String.format("%02d:%02d",
+                fecha.getHora().getHora(),
+                fecha.getHora().getMinuto());
+    }
+
+    @SuppressWarnings("unused")
+    private FechaHora parsearFecha(String fecha) {
+        int dia, mes, anio;
+        String[] valores = fecha.split("\\/");
+        dia = Integer.parseInt(valores[0]);
+        mes = Integer.parseInt(valores[1]);
+        anio = Integer.parseInt(valores[2]);
+        FechaHora fechaHora = new FechaHora(dia, mes, anio, 0, 0);
+        return fechaHora;
+    }
+
+    private FechaHora parsearFecha(String fecha, String hora) {
+        int dia, mes, anio;
+        String[] valores = fecha.split("\\/");
+        dia = Integer.parseInt(valores[0]);
+        mes = Integer.parseInt(valores[1]);
+        anio = Integer.parseInt(valores[2]);
+        int minuto, segundo;
+        valores = hora.split("\\:");
+        minuto = Integer.parseInt(valores[0]);
+        segundo = Integer.parseInt(valores[1]);
+        FechaHora fechaHora = new FechaHora(dia, mes, anio, minuto, segundo);
+        return fechaHora;
+    }
 }
